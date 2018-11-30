d2dcb0e45f3af79496bf2ab3e3c1a452e6b49893
Conner Tennery <ctennery.dev@gmail.com>
2018-11-30 04:00:23 -0600

    Count message test
    Minor refactoring to just count messages in a channel

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+var badElements = [];
+var messageList = []; //Can I write to a file using Discord's libraries..? This is going to be gigantic.
+
+var scrl = document.getElementsByClassName('messages-3amgkR scroller')[0];
+// scrl.scrollTop = 0;
+var arr = Array.from(document.getElementsByClassName('markup-2BOw-j isCompact-1hsne1')); //This class name can change. It was "body" and now it's "markup-2BOw-j isCompact-1hsne1"
+
+//Compact view structure
 class Message {
 	constructor(a, m) {
 		this.author = a;
@@ -19,43 +27,45 @@ class Message {
 			badElements.push(element);
 			return;
 		}
-		if (!element.children[0].children[1].children[0].children[0]) {
-			console.error(element);
-			badElements.push(element);
-			return;
-		}
 
-		if (!element.children[1]) {
+		if (!element.innerText) {
 			console.error(element);
 			badElements.push(element);
 			return;
 		}
-		if (!element.children[1].children[0]) {
-			console.error(element);
-			badElements.push(element);
-			return;
-		}
-		if (!element.children[1].children[0].children[1]) {
-			console.error(element);
-			badElements.push(element);
-			return;
-		}
-		return new Message(
-			element.children[0].children[1].children[0].children[0].innerText,
-			element.children[1].children[0].children[1].innerText
-		);
+		return new Message(element.children[0].children[1].children[0].innerText, element.innerText);
 	}
 }
-var badElements = [];
-var x = new Message('test', 'message');
 
-var messageList = []; //Can I write to a file using Discord's libraries..? This is going to be gigantic.
+var keepLooking = true;
+function countMessages() {
+	if (!keepLooking) return;
 
-var scrl = document.getElementsByClassName('messages-3amgkR scroller')[0];
-// scrl.scrollTop = 0;
-var arr = Array.from(
-	document.getElementsByClassName('messageCozy-2JPAPA message-1PNnaP')
-); //This class name can change. It was "body" and now it's "messageCozy-2JPAPA message-1PNnaP"
+	var arr = Array.from(document.getElementsByClassName('markup-2BOw-j isCompact-1hsne1'));
+	arr.reverse().forEach(x => {
+		if (x.hasBeenRead) return;
+		var message = Message.HTMLToMessage(x);
+		if (!message) {
+			keepLooking = false;
+			console.error('Unable to make message');
+		}
+		// console.log(message.author + ': ' + message.message);
+		messageList.push(message);
+		x.hasBeenRead = true;
+	});
+	scrl.scrollTop = 0;
+	if (messageList.length > 10000) {
+		keepLooking = false;
+		console.error('10,000+ messages');
+	}
+	// loop -= 1;
+
+	window.setTimeout(function() {
+		countMessages();
+	}, 8000);
+}
+
+countMessages();
 
 //The structure of a message change change often
 //TODO: Find a way to determine structure of messages from the start then utilize that.
@@ -83,29 +93,3 @@ var arr = Array.from(
 // 	if (messageList.length > 100) keepLooking = false;
 // 	loop -= 1;
 // }
-
-var keepLooking = true;
-function grabCurrentMessages() {
-	if (!keepLooking) return;
-
-	var arr = Array.from(
-		document.getElementsByClassName('messageCozy-2JPAPA message-1PNnaP')
-	);
-	arr.reverse().forEach(x => {
-		if (x.hasBeenRead) return;
-		var message = Message.HTMLToMessage(x);
-		if (!message) return;
-		console.log(message.author + ': ' + message.message);
-		messageList.push(message);
-		x.hasBeenRead = true;
-	});
-	scrl.scrollTop = 0;
-	if (messageList.length > 10000) keepLooking = false;
-	// loop -= 1;
-
-	window.setTimeout(function() {
-		grabCurrentMessages();
-	}, 8000);
-}
-
-grabCurrentMessages();
